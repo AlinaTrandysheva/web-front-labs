0d1df8e691de8fbfea468a2c2364a6d5aeef194d
Трандышева Алина <trandyshevaalina@gmail.com>
2025-05-12 19:48:34 +0300

    Книги

---
+++ lab6/book.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Фёдор Достоевский',
+        name: 'Преступление и наказание',
+        publicationYear: 1866,
+        price: 550,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Лев Толстой',
+        name: 'Война и мир',
+        publicationYear: 1869,
+        price: 690,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author: 'Александр Пушкин',
+        name: 'Евгений Онегин',
+        publicationYear: 1833,
+        price: 430,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Михаил Булгаков',
+        name: 'Мастер и Маргарита',
+        publicationYear: 1967,
+        price: 610,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Николай Гоголь',
+        name: 'Мёртвые души',
+        publicationYear: 1842,
+        price: 490,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author: 'Антон Чехов',
+        name: 'Палата №6',
+        publicationYear: 1892,
+        price: 360,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Ильф и Петров',
+        name: 'Двенадцать стульев',
+        publicationYear: 1928,
+        price: 520,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Михаил Шолохов',
+        name: 'Судьба человека',
+        publicationYear: 2015,
+        price: 380,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Владимир Набоков',
+        name: 'Защита Лужина',
+        publicationYear: 1930,
+        price: 600,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Сергей Довлатов',
+        name: 'Заповедник',
+        publicationYear: 1983,
+        price: 450,
+        bestseller: false,
+        inStock: false
+    }
+];
